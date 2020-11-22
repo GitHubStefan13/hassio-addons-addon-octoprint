5e97753ecb70a73ad6170d48e3df2275139ac344
Stefan Unruh <dsgamingdev@gmail.com>
2020-11-22 14:04:48 +0000

    Permission for config Folder

--- octoprint/run.sh
+++ octoprint/run.sh
@@ -3,5 +3,10 @@
 echo "Parsing Options ..."
 CONFIG_PATH=/data/options.json
 
+echo "Set Permissions for OctoPrint config Path"
+mkdir -p /config/octoprint
+chown root:octoprint /config/octoprint
+chmod 775 /config/octoprint
+
 echo "Starting OctoPrint"
 su -c "/home/octoprint/startOctoPrint.sh" octoprint
